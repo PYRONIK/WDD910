e5ae18494407a4579e8e86630f8e99ab73ee9a1f
Christopher Schwarz <iwsc00@gmail.com>
2012-02-06 21:36:17 +0100

    JSON

---
+++ 008_ajax_json.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+$anmeldungen = Array();
+
+$anmeldungen[0] = Array('name' => 'Franz', 'email' => 'test@test.at', 'update' => '1.1.2000');
+$anmeldungen[1] = Array('name' => 'Franz', 'email' => 'test@test.at', 'update' => '1.1.2000');
+$anmeldungen[2] = Array('name' => 'Franz', 'email' => 'test@test.at', 'update' => '1.1.2000');
+$anmeldungen[3] = Array('name' => 'Franz', 'email' => 'test@test.at', 'update' => '1.1.2000');
+$anmeldungen[4] = Array('name' => 'Franz', 'email' => 'test@test.at', 'update' => '1.1.2000');
+
+
+
+echo json_encode($anmeldungen);
+
+?>

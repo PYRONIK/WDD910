79a959f4edd9c7fd75b6a2cda9f6760fea557a0c
Christopher Schwarz <iwsc00@gmail.com>
2012-04-30 22:58:33 +0200

    kommentare

--- OOP/oopphp/index.php
+++ OOP/oopphp/index.php
@@ -1,23 +1,29 @@
 <?php
-
+/* Anzeige aller Fehlermeldungen aktivieren */
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 
 
 // require('class/pkw.class.php');
 
+/* __autoload wird bei jeder Instanzierung aufgerufen */
 function __autoload($class){
 	require('class/'.$class.'.class.php');
 }
 
+// neuen roten pkw erstellen
 $myauto =  new pkw('blau');
 
+// Sportwagen-Instanz bilden
 $myauto2 = new sportwagen('rot');
 
+
+// pkw bremst
 $myauto -> bremsen();
 
 echo "<hr />";
 
+// sportwagen bremst
 $myauto2 ->bremsen();
 ?>
 

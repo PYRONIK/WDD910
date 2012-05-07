b873ddc5084f739d0970729a8320ec11583fee18
Christopher Schwarz <iwsc00@gmail.com>
2012-05-07 20:59:47 +0200

    -

--- OOP/userclass/class/class.user.php
+++ OOP/userclass/class/class.user.php
@@ -10,11 +10,26 @@ class user{
 	private $_dbConnection;
 
 	public function __construct(){
-	
+		// login
+		if( isset($_POST['username']) && isset($_POST['password']) ){
+			return $this->login($_POST['username'], $_POST['password']); 
+		}
+
+		// logout
+		if( isset($_GET['logout']) ){
+			return $this->logout();
+		}
+
+
+		if( isset( $_SESSION['username'] ) && isset( $_SESSION['password'] ) && isset( $_SESSION['group'] )){
+		
+		}else{
+			$this->_setUsername(false,false,false);
+		}
 	}
 
 	public function login($username, $password){
-	
+		echo $username . $password;
 	}
 
 	private function _checkUser(){
